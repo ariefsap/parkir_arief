ab028190892eac7f5aaf92aa4780fbcf3d81a331
anasbayu <anasbayu03@gmail.com>
2017-11-16 16:32:15 +0700

    update service register

--- parking/service/register.php
+++ parking/service/register.php
@@ -1,5 +1,6 @@
 <?php
     include '../inc/koneksi.php';
+    error_reporting(0);
 
     $namaPengguna = $_POST["nama_pengguna"];
     $username = $_POST["username"];
@@ -7,40 +8,42 @@
     $confirmPassword = $_POST["password_konfirmasi"];
     $foto = "";
     $idJenisPengguna = $_POST["id_jenis_pengguna"];
+    $alamat = $_POST["alamat"];
     $response = new stdClass();
 
     if (empty($username)) {
         $response->status = false;
         $response->pesan = "Kolom username tidak boleh kosong";
-        echo json_encode($response);
+        $response->data = new stdClass();
     } else if (empty($password)) {
         $response->status = false;
         $response->pesan = "Kolom password tidak boleh kosong";
-        echo json_encode($response);
+        $response->data = new stdClass();
     } else if (empty($confirmPassword) || $password != $confirmPassword) {
         $response->status = false;
         $response->pesan = "Konfirmasi password tidak sama";
-        echo json_encode($response);
+        $response->data = new stdClass();
     } else {
-        if (!empty($username) && $password == $confirmPassword){
+        if (!empty($username) && ($password == $confirmPassword) && !empty($alamat)){
             $query = mysql_query("INSERT INTO pengguna (nama_pengguna, alamat,
                                 username, password, foto, id_jenis_pengguna)
                                 VALUES('$namaPengguna', '$alamat', '$username',
-                                    '$password', '$foto', '$idJenisPengguna')"));
+                                    '$password', '$foto', '$idJenisPengguna')");
 
             if ($query){
                 $response = new stdClass();
                 $response->status = true;
                 $response->pesan = "Register berhasil, silahkan login.";
-                echo json_encode($response);
+                $response->data = new stdClass();
             } else {
                 $response = new stdClass();
                 $response->status = false;
                 $response->pesan = "Terjadi kesalahan";
-                echo json_encode($response);
+                $response->data = new stdClass();
             }
         }
     }
+    echo json_encode($response);
 
     mysql_close();
 ?>
